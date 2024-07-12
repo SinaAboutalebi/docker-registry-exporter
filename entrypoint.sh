f38497c404627775b23175db561ccc2e6deb8245
Zer0Power <zeroopoower@gmail.com>
2024-07-12 15:14:40 +0330

    fix: correct spacing in docker-registry-exporter script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-exec ./docker-registry-exporter\ 
- --registry-address=$REGISTRY_ADDRESS\
+exec ./docker-registry-exporter \
+ --registry-address=$REGISTRY_ADDRESS \
  --listen-address=0.0.0.0:9055
